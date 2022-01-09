ca4a5b387c38161129bc84a81893f957e16b4267
Martin <martinvidal.n7@gmail.com>
2022-01-08 21:33:21 -0300

    app.js

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ app.use(express.static(path.resolve(__dirname,"./public")))
 
 app.get("/", (req, res) => res.sendFile(path.resolve(__dirname,"./views","home.html")))
 
-app.get("/registrar", (req, res) => res.sendFile(path.resolve(__dirname,"./views","registrar.html")))
+app.get("/registro", (req, res) => res.sendFile(path.resolve(__dirname,"./views","registro.html")))
 
 app.get("/ingresar", (req, res) => res.sendFile(path.resolve(__dirname,"./views","ingresar.html")))
 
 app.get("/carrito", (req, res) => res.sendFile(path.resolve(__dirname,"./views","carrito.html")))
 
 app.get("/nosotros", (req, res) => res.sendFile(path.resolve(__dirname,"./views","nosotros.html")))
+
+app.get("/producto", (req, res) => res.sendFile(path.resolve(__dirname,"./views","producto.html")))
