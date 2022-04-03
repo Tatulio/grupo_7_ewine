d12fc49e323d0305bc8ddf5ccab061ac2de6a701
Martin Vidal Freyre <martinvidal.n7@gmail.com>
2022-04-03 20:03:16 -0300

    cosas del carrito

--- src/controllers/carrito.js
+++ src/controllers/carrito.js
@@ -1,6 +1,30 @@
+const product = require("../models/product")
+const file = require("../models/file")
+
 module.exports = {
-    carrito: (req,res) => res.render("carrito", {
-        styles: ["carrito"],
-        title: "Carrito de Compras"
-    })
+    carrito: (req,res) => {
+        product.all()
+        .then((resultado) => {
+            res.render("carrito", {
+                styles: ["carrito"],
+                title: "Carrito de Compras",
+                products: resultado})
+            })
+    },
+
+    show: (req,res) => {
+        product.search("id", req.params.id).then((resultado) => {
+            return resultado ? 
+            res.render("vino", {
+            styles: ["vino"],
+            title: "Vino | " +resultado.name,
+            product: resultado 
+
+        }) : res.render("error",{
+            msg: "Producto no encontrado"
+        })
+    })}
+
+
+
 }
